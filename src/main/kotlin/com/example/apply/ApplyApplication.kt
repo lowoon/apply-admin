c483caa622de5b3ffa15bd9a2784a944af3f386c
lowoon <sangbo0229@gmail.com>
2020-09-01 17:30:43 +0900

    init

---
+++ src/main/kotlin/com/example/apply/ApplyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.apply
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApplyApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApplyApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/apply/ApplyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.apply
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
